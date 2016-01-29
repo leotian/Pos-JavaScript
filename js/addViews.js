70b4090eff64a4a9299f08e3fab26030d3293968
Leo Tian <tian4010292@outlook.com>
2016-01-29 10:08:40 +0800

    更改addWelcome.js名为addView.js

--- js/addViews.js
+++ js/addViews.js
@@ -14,3 +14,9 @@ $(document).ready(function(){
     $("#header").load("views/header.html");
     $("#view").load("views/home.html");
 });
+
+
+$(document).ready(function() {
+    if(document.title=="主页") $("ul li:eq(0)").addClass("active");
+    if(document.title=="商品列表页") $("ul li:eq(1)").addClass("active");
+})
