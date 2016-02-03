3e1642af9ae185fee40f4b241d8ec7f5af5a3506
Leo Tian <tian4010292@outlook.com>
2016-02-03 17:42:41 +0800

    实现goodlist页面的table内动态添加商品和button

--- js/addGoods.js
+++ js/addGoods.js
@@ -2,12 +2,49 @@
  * Created by tyb on 16-1-28.
  */
 
+$(document).ready(function(){
+    listController();
+    addGiftFly();
+})
+
+function listController(){
+    var items = loadAllItems();
+    items.forEach(function(item){
+        var rowItem = itemRowHelper(item);
+        $(".table").append(rowItem);
+    });
+}
+
+function listListener () {
+    $('.list-item').on('click', 'button', function () {
+        var itemName = $(this).closest('.list-item').find('.item-name').text();
+        var item = Order.findByName(itemName);
+        item.addCount();
+        cartCountInitiate();
+    });
+}
+
+function itemRowHelper(item){
+    var itemRow = '<tr class="list-item"><td class="item-type"></td><td class="item-name"></td><td class="item-price"></td><td class="item-unit"></td><td class="item-buy"></td></tr>';
+    var itemDom = $(itemRow);
+    itemDom.find('.item-type').text(item.type);
+    itemDom.find('.item-name').text(item.name);
+    itemDom.find('.item-price').text(item.price);
+    itemDom.find('.item-unit').text(item.unit);
+    itemDom.find('.item-buy').html(buyButtonHelper());
+    return itemDom;
+}
+
+function buyButtonHelper () {
+    return '<button class="btn btn-primary btm-sm">加入购物车</button>';
+}
 
 function addGiftFly(){
     var offset = $("#end").offset();
     $(".btn").click(function(event){
         var btn = $(this);
-        var flight = $('<img class="flight" src="img/gift.png">');
+        console.log('a');
+        var flight = $('<img class="flight" src="../img/gift.png">');
         flight.fly({
             start: {
                 left: event.pageX,
@@ -26,6 +63,3 @@ function addGiftFly(){
         });
     });
 }
-
-
-
